23e6198883285a3b8d4b1be95b6b175c1248366c
Oussama <sohaib.ghazi@outlook.fr>
2024-09-28 22:39:59 +0100

    refactor: Add react-intersection-observer library for intersection tracking

--- src/app/statistics.tsx
+++ src/app/statistics.tsx
@@ -1,13 +1,15 @@
 "use client";
 
+import { useState } from "react";
 import CountUp from "react-countup";
+import { useInView } from "react-intersection-observer";
 import Container from "./components/container";
 
 const statistics = [
   {
     id: "countto2",
     value: 350,
-    label: "Trusted Clinets",
+    label: "Trusted Clients",
   },
   {
     id: "countto3",
@@ -18,20 +20,30 @@ const statistics = [
   {
     id: "countto4",
     value: 215,
-    label: "finished projects",
+    label: "Finished Projects",
   },
   {
-    id: "countto4",
+    id: "countto5",
     value: 3,
     prefix: "+",
     suffix: "M",
-    label: "annual profits",
+    label: "Annual Profits",
   },
 ];
 
 export default function Statistics() {
+  const [hasAnimated, setHasAnimated] = useState(false);
+  const { ref } = useInView({
+    triggerOnce: true,
+    onChange: (inView) => {
+      if (inView) {
+        setHasAnimated(true);
+      }
+    },
+  });
+
   return (
-    <div className="bg-gray-100 py-6">
+    <div className="bg-gray-100 py-6" ref={ref}>
       <Container>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 justify-items-center">
           {statistics.map((stat) => (
@@ -39,12 +51,14 @@ export default function Statistics() {
               <div className="flex items-center gap-3 p-6">
                 <h3 className="text-6xl font-bold leading-tight text-center text-primary ">
                   {stat.prefix}
-                  <CountUp
-                    end={stat.value}
-                    decimals={stat.decimals || 0}
-                    duration={2.5}
-                    separator=","
-                  />
+                  {hasAnimated && (
+                    <CountUp
+                      end={stat.value}
+                      decimals={stat.decimals || 0}
+                      duration={2.5}
+                      separator=","
+                    />
+                  )}
                   {stat.suffix}
                 </h3>
                 <p className="mt-2 text-base font-semibold text-muted-foreground uppercase leading-tight">
