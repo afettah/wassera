619b0cbaf0596265b467867c9733bb8214294942
afettah <afettah.ghazi@gmail.com>
2024-09-29 21:57:57 +0200

    fix conflict

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -10,29 +10,28 @@ import ChatbotScript from './ChatbotScript';
 // Define Gotham with different weights
 const gotham = localFont({
   src: [
-    { path: "../fonts/Gotham-Light.otf", weight: "300", style: "normal" },
-    { path: "../fonts/Gotham-Book.otf", weight: "400", style: "normal" },
-    { path: "../fonts/Gotham-Medium.otf", weight: "500", style: "normal" },
-    { path: "../fonts/Gotham-Bold.otf", weight: "700", style: "normal" },
-    { path: "../fonts/Gotham-Ultra.otf", weight: "800", style: "normal" },
+    { path: '../fonts/Gotham-Light.otf', weight: '300', style: 'normal' },
+    { path: '../fonts/Gotham-Book.otf', weight: '400', style: 'normal' },
+    { path: '../fonts/Gotham-Medium.otf', weight: '500', style: 'normal' },
+    { path: '../fonts/Gotham-Bold.otf', weight: '700', style: 'normal' },
+    { path: '../fonts/Gotham-Ultra.otf', weight: '800', style: 'normal' },
   ],
-  variable: "--font-gotham",
+  variable: '--font-gotham',
 });
 
 // Define GE SS with different weights
 const geSS = localFont({
   src: [
-    { path: "../fonts/GE SS Light.otf", weight: "300", style: "normal" },
-    { path: "../fonts/GE SS Medium.otf", weight: "500", style: "normal" },
-    { path: "../fonts/GE SS Bold.otf", weight: "700", style: "normal" },
+    { path: '../fonts/GE SS Light.otf', weight: '300', style: 'normal' },
+    { path: '../fonts/GE SS Medium.otf', weight: '500', style: 'normal' },
+    { path: '../fonts/GE SS Bold.otf', weight: '700', style: 'normal' },
   ],
-  variable: "--font-ge-ss",
+  variable: '--font-ge-ss',
 });
 
 export const metadata: Metadata = {
-  title: "Wassera Mortgage and Finance",
-  description:
-    "A FinTech company specialised in Mortgage, Investment and Savings Services",
+  title: 'Wassera Mortgage and Finance',
+  description: 'A FinTech company specialised in Mortgage, Investment and Savings Services',
 };
 
 export default function RootLayout({
@@ -44,22 +43,18 @@ export default function RootLayout({
 }>) {
   const { direction: dir } = new Locale(locale).textInfo;
 
-  const isArabic = locale?.startsWith("ar");
+  const isArabic = locale?.startsWith('ar');
 
   return (
     <html lang={locale} dir={dir}>
-      <body
-        className={`${
-          isArabic ? geSS.variable : gotham.variable
-        } antialiased flex flex-col bg-transparent`}
-      >
+      <body className={`${isArabic ? geSS.variable : gotham.variable} antialiased flex flex-col bg-transparent`}>
         <I18nProviderClient locale={locale}>
           <TopBar />
           <TopMenu />
           {children}
-          <ChatbotScript />
-        </body>
-      </html>
-    </I18nProviderClient>
+        </I18nProviderClient>
+        <ChatbotScript />
+      </body>
+    </html>
   );
 }
