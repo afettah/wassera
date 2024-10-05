c846bc3cfaae27806c375f9877fe7e29b836a2f4
aghazi <afettah.ghazi@gmail.com>
2024-10-05 22:16:29 +0200

    fix team section and clean images

--- src/app/[locale]/about/BoardOfDirectorsSection.tsx
+++ src/app/[locale]/about/BoardOfDirectorsSection.tsx
@@ -1,76 +1,78 @@
-"use client"
+'use client';
 
-import { useEffect, useRef } from 'react'
-import Image from 'next/image'
-import { motion, useInView, useAnimation } from 'framer-motion'
+import { useEffect, useRef } from 'react';
+import Image from 'next/image';
+import { motion, useInView, useAnimation } from 'framer-motion';
+import { useCurrentLocale, useScopedI18n } from '@/locales/client';
 
 type Director = {
-  name: string
-  image: string
-}
-
-// path of images public/images/persons
+  name: string;
+  image: string;
+  arabic: string;
+};
 
 const directors: Director[] = [
-  { name: 'Naif Al-Watbaan', image: '/images/persons/person1.png' },
-  { name: 'Mohammed Al-Eisa', image: '/images/persons/person2.png' },
-  { name: 'Abdullah Al-Shaikh', image: '/images/persons/person3.png' },
-  { name: 'Mohammed Al-Malki', image: '/images/persons/person4.png' },
-  { name: 'Abdulaziz Al Harbi', image: '/images/persons/person5.png' },
-  { name: 'Bassam AlBassam', image: '/images/persons/person6.png' },
-]
+  { name: 'Naif Al-Watbaan', image: '/images/team/Naif Al-Watbaan.png', arabic: 'نايف الوطبان' },
+  { name: 'Mohammed Al-Eisa', image: '/images/team/Mohammed Al-Eisa.png', arabic: 'محمد العيسى' },
+  { name: 'Abdullah Al-Shaikh', image: '/images/team/Abdullah Al-Shaikh.png', arabic: 'عبدالله الشيخ' },
+  { name: 'Mohammed Al-Malki', image: '/images/team/Mohammed Al-Malki.png', arabic: 'محمد المالكي' },
+  { name: 'Abdulaziz Al Harbi', image: '/images/team/Abdulaziz Al Harbi.png', arabic: 'عبدالعزيز الحربي' },
+  { name: 'Bassam AlBassam', image: '/images/team/Bassam AlBassam.png', arabic: 'بسام البسام' },
+];
 
 export default function BoardOfDirectorsSection() {
-  const controls = useAnimation()
-  const ref = useRef(null)
-  const isInView = useInView(ref, { once: true, amount: 0.2 })
+  const aboutT = useScopedI18n('about');
+  const locale = useCurrentLocale();
+  const controls = useAnimation();
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true, amount: 0.2 });
 
   useEffect(() => {
     if (isInView) {
-      controls.start("visible")
+      controls.start('visible');
     }
-  }, [controls, isInView])
+  }, [controls, isInView]);
 
   return (
     <section ref={ref} className="py-12 ">
-      <div >
-        <motion.h2 
+      <div>
+        <motion.h2
           className="text-3xl font-bold mb-12 text-center"
           initial={{ opacity: 0, y: 20 }}
           animate={controls}
           transition={{ duration: 0.7 }}
           variants={{
-            visible: { opacity: 1, y: 0 }
+            visible: { opacity: 1, y: 0 },
           }}
         >
-          Board of Directors
+          {aboutT('directors.title')}
         </motion.h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
           {directors.map((director, index) => (
-            <motion.div 
-              key={director.name} 
+            <motion.div
+              key={director.name}
               className="flex flex-col items-center"
               initial={{ opacity: 0, y: 50 }}
               animate={controls}
               transition={{ duration: 0.5, delay: index * 0.2 }}
               variants={{
-                visible: { opacity: 1, y: 0 }
+                visible: { opacity: 1, y: 0 },
               }}
             >
               <div className="relative w-48 h-48 mb-4 overflow-hidden rounded-full">
                 <Image
                   src={director.image}
                   alt={director.name}
-                  layout="fill"
-                  objectFit="cover"
+                  fill
+                  style={{ objectFit: 'cover' }}
                   className="transition-transform duration-300 hover:scale-110"
                 />
               </div>
-              <h3 className="text-xl font-semibold text-center">{director.name}</h3>
+              <h3 className="text-xl font-semibold text-center">{locale === 'ar' ? director.arabic : director.name}</h3>
             </motion.div>
           ))}
         </div>
       </div>
     </section>
-  )
+  );
 }
--- src/app/[locale]/about/FounderSection.tsx
+++ src/app/[locale]/about/FounderSection.tsx
@@ -1,112 +1,138 @@
-"use client"
+'use client';
 
-import Image from "next/image"
-import { CardContent, CardHeader, CardTitle } from "@/components/ui/card"
-import { MagicCard } from "@/components/ui/magic-card"
-import { motion } from "framer-motion"
-import { useInView } from 'react-intersection-observer'
+import Image from 'next/image';
+import { CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { MagicCard } from '@/components/ui/magic-card';
+import { motion } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
+import { useCurrentLocale, useScopedI18n } from '@/locales/client';
 
-const foundersData = [
+type Founder = {
+  name: {
+    en: string;
+    ar: string;
+  };
+  position: 'founder' | 'coFounder';
+  description: {
+    en: string;
+    ar: string;
+  };
+  image: string;
+};
+
+const foundersData: Founder[] = [
   {
-    name: "Abdulelah Aloshayni",
-    title: "Founder",
-    description: "14 years of Technology and digital transformation expertise holding an executive MBA.",
-    image: "/images/persons/person6.png"
+    name: {
+      en: 'Abdulelah Aloshayni',
+      ar: 'عبدالإله العشيني',
+    },
+    position: 'founder',
+    description: {
+      en: '14 years of Technology and digital transformation expertise holding an executive MBA.',
+      ar: '١٨ عاماً من الخبرة في التقنية والتحول الرقمي. حاصل على الماجستير في إدارة الأعمال التنفيذية.',
+    },
+    image: '/images/team/Abdulelah Aloshayni.png',
   },
   {
-    name: "Saleh AlGhamdi",
-    title: "Co-Founder",
-    description: "17 years of experience in information technology & communications infrastructure.",
-    image: "/placeholder.svg"
+    name: { en: 'Saleh AlGhamdi', ar: 'صالح الغامدي' },
+    position: 'coFounder',
+    description: {
+      en: '17 years of experience in information technology & communications infrastructure.',
+      ar: '١٨ عاماً من الخبرة في تقنية المعلومات والبنية التحتية للاتصالات.',
+    },
+    image: '/images/team/Saleh AlGhamdi.png',
   },
   {
-    name: "Khalid Alzahrani",
-    title: "Co-Founder",
-    description: "18 years of diverted experiences in many industries with strategic planning, development, and transformation focus.",
-    image: "/placeholder.svg"
+    name: { en: 'Khalid Alzahrani', ar: 'خالد الزهراني' },
+    position: 'coFounder',
+    description: {
+      en: '18 years of diverted experiences in many industries with strategic planning,  development, and transformation focus.',
+      ar: '١٨ عاماً من الخبرات المتنوعة في العديد من الصناعات، مع التركيز على التخطيط الاستراتيجي والتطوير والتحول.',
+    },
+    image: '/images/team/Khalid Alzahrani.png',
   },
   {
-    name: "Saud Alharbi",
-    title: "Co-Founder",
-    description: "17 years of IT experience with extensive knowledge of infrastructure & processes.",
-    image: "/placeholder.svg"
+    name: { en: 'Saud Alharbi', ar: 'سعود الحربي' },
+    position: 'coFounder',
+    description: {
+      en: '17 years of IT experience with extensive knowledge of infrastructure & processes.',
+      ar: '١٨ عاماً من الخبرة في تقنية المعلومات مع معرفة واسعة بالبنية التحتية والعمليات.',
+    },
+    image: '/images/team/Saud Alharbi.png',
   },
   {
-    name: "Abdulrahman Alkhalifa",
-    title: "Co-Founder",
-    description: "16 years of experience in marketing and sales in both private and government sectors.",
-    image: "/placeholder.svg"
+    name: { en: 'Abdulrahman Alkhalifa', ar: 'عبدالرحمن الخليفة' },
+    position: 'coFounder',
+    description: {
+      en: '16 years of experience in marketing and sales in both private and government sectors.',
+      ar: '١٨ عاماً من الخبرة في التسويق والمبيعات في كل من القطاعين الخاص والحكومي.',
+    },
+    image: '/images/team/Abdulrahman Alkhalifa.png',
   },
   {
-    name: "Jubran Alshahrani",
-    title: "Co-Founder",
-    description: "15 years of experience in diversified experiences in information technology, shared services, excellence management, new companies building up, governance and transformation. Holding Executive MBA.",
-    image: "/placeholder.svg"
+    name: { en: 'Jubran Alshahrani', ar: 'جبران الشهراني' },
+    position: 'coFounder',
+    description: {
+      en: '15 years of experience in diversified experiences in information technology, shared services, excellence management, new companies building up, governance and transformation. Holding Executive MBA.',
+      ar: '١٨ عاماً من الخبرة في مجالات متنوعة تجارب في إدارة الشركات، التكنولوجيا، الخدمات المشتركة، والتحول. عقد ماجستير في إدارة الأعمال التنفيذية.',
+    },
+    image: '/images/team/Jubran Alshahrani.png',
   },
   {
-    name: "Amjad Shwali",
-    title: "Co-Founder",
-    description: "16 years of experience, in banking, treasury, and Investment for government & semi-gov entities.",
-    image: "/placeholder.svg"
-  }
-]
+    name: { en: 'Amjad Shwali', ar: 'أمجد شويل' },
+    position: 'coFounder',
+    description: {
+      en: '16 years of experience, in banking, treasury, and Investment for government & semi-gov entities.',
+      ar: '١٨ عاماً من الخبرة في مجال المصرفية والخزينة والاستثمار في العديد من الجهات الحكومية وشبه الحكومية.',
+    },
+    image: '/images/team/Amjad Shwail.png',
+  },
+];
 
 const cardVariants = {
   hidden: { opacity: 0, y: 50 },
   visible: { opacity: 1, y: 0 },
-}
+};
 
 export default function FoundersSection() {
+  const aboutT = useScopedI18n('about');
+  const locale = useCurrentLocale();
   const { ref, inView } = useInView({
     triggerOnce: true,
     threshold: 0.1,
-  })
+  });
 
   return (
     <div aria-labelledby="founders-heading" className="relative">
-      <Image
-        src="/placeholder.svg"
-        alt="Abstract shapes"
-        width={600}
-        height={600}
-        className="absolute right-0 top-1/2 -translate-y-1/2 opacity-10"
-      />
       <div className="relative z-10">
-        <h2
-          id="founders-heading"
-          className="text-4xl font-semibold mb-6 text-center"
-        >
-          Our Founders
+        <h2 id="founders-heading" className="text-4xl font-semibold mb-6 text-center">
+          {aboutT('founders.title')}
         </h2>
-        <p className="text-center mb-12 text-lg max-w-3xl mx-auto">
-          Meet the visionaries behind our company, bringing years of expertise and innovation to drive our mission forward.
-        </p>
+        <p className="text-center mb-12 text-lg max-w-3xl mx-auto">{aboutT('founders.description')}</p>
         <div ref={ref} className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {foundersData.map((founder, index) => (
             <motion.div
-              key={founder.name}
+              key={founder.name.en}
               initial="hidden"
-              animate={inView ? "visible" : "hidden"}
+              animate={inView ? 'visible' : 'hidden'}
               variants={cardVariants}
               transition={{ duration: 0.5, delay: index * 0.2 }}
             >
-              <FounderCard {...founder} />
+              <FounderCard
+                image={founder.image}
+                name={founder.name[locale]}
+                title={aboutT(`founders.positions.${founder.position}`)}
+                description={founder.description[locale]}
+              />
             </motion.div>
           ))}
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-interface FounderCardProps {
-  image: string
-  name: string
-  title: string
-  description: string
-}
-
-function FounderCard({ image, name, title, description }: FounderCardProps) {
+function FounderCard({ image, name, title, description }: { image: string; name: string; title: string; description: string }) {
   return (
     <MagicCard
       className="relative overflow-hidden group hover:shadow-lg transition-shadow duration-300"
@@ -118,23 +144,15 @@ function FounderCard({ image, name, title, description }: FounderCardProps) {
         <div className="relative w-32 h-32 mx-auto mb-4">
           <div className="absolute inset-0 bg-primary transform rotate-45 scale-110"></div>
           <div className="relative z-10 w-full h-full rounded-full overflow-hidden">
-            <Image
-              src={image}
-              alt={name}
-              layout="fill"
-              objectFit="cover"
-              className="transition-transform duration-300 group-hover:scale-110"
-            />
+            <Image src={image} alt={name} fill style={{ objectFit: 'cover' }} className="transition-transform duration-300 group-hover:scale-110" />{' '}
           </div>
         </div>
         <CardTitle className="text-xl text-center">{name}</CardTitle>
         <p className="text-center text-primary font-medium">{title}</p>
       </CardHeader>
       <CardContent>
-        <p className="text-muted-foreground group-hover:text-foreground transition-colors duration-300 text-center">
-          {description}
-        </p>
+        <p className="text-muted-foreground group-hover:text-foreground transition-colors duration-300 text-center">{description}</p>
       </CardContent>
     </MagicCard>
-  )
+  );
 }
